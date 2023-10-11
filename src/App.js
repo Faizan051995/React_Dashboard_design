8805760b49e9064c32c4675a05dfc40242581e08
aadilsid <Hp@DESKTOP-P0BB9G8>
2023-10-11 19:51:38 +0500

    create react desboard template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import '../src/App.css'
+import { Col, Image, Layout, Menu, Row } from 'antd';
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+} from '@ant-design/icons';
+import rendomImage from "./images/rendomimage.png"
+import DashboardLayout from './components/DashboardLayout';
+const { Sider, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+
+<DashboardLayout/>
+    </>
+  )
 }
 
-export default App;
+export default App
